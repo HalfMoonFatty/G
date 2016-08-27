d320ed1b77288ad6451a07f1a49ead7ba928502e
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-27 14:37:17 -0700

    Update ParkingLot.py

--- ParkingLot.py
+++ ParkingLot.py
@@ -14,3 +14,26 @@ Example:
 src={1,0,2,3}; tgt={0,2,3,1};
 
 '''
+def recoverArr(src,tar):
+
+	ind = {item:i for i, item in enumerate(tar)}
+
+	for i in range(len(tar)):
+		if tar[i] == src[i]:
+			continue
+
+		# swap current with 0
+		tar[i],tar[ind[0]] = tar[ind[0]],tar[i]
+		ind[0],ind[tar[i]] = i,ind[0]
+
+		# swap 0 with the correct number
+		tar[i],tar[ind[src[i]]] = tar[ind[src[i]]],tar[i]
+		ind[0],ind[src[i]] = ind[src[i]],i
+
+
+	return 
+
+src = [0,1,2,3]
+tar = [2,0,3,1]
+recoverArr(src,tar)
+print tar

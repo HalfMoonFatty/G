06c2d025f3c82f0091af1a40c9a3d5ef4c18e673
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-27 12:05:16 -0700

    Create EmpleyeeRating.py

---
+++ EmpleyeeRating.py
@@ -0,0 +1,10 @@
+'''
+Considering there are N engineers in one company with id from 1 to N, Each of the Engineer has a skill rate R_i, 
+we want to select one group of engineering whose ids have to be continuous. 
+The size of one group is the number of engineers in it and the skill rating of the group is the lowest rating of all the members in the group. 
+Now given N and array R, for each group from size X from 1 to N, we want to know the highest skill rating of all the groups whose size is X。 
+'''
+
+'''
+Sliding Window - deque
+'''

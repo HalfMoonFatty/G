3cd8aaef683982d6ed80f11b8334e5ce6cd2fca0
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-27 14:58:24 -0700

    Create [x,x+1).py

---
+++ [x,x+1).py
@@ -0,0 +1,9 @@
+'''
+Problem:
+
+Given a set S of 10^6 doubles. Find a number X so that the [X, X+1) half-open real interval contains as many elements of S as possible.
+For example, given this subset: 2.7, 0.23, 8.32, 9.65, -6.55, 1.55, 1.98, 7.11, 0.49, 2.75, 2.95, -96.023, 0.14, 8.60, 
+the value X desired is 1.98 because there are 4 values in the range 1.98 to 2.97999 (1.98, 2.7, 2.75, 2.95)
+
+'''
+

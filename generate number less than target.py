097183debc0e28c04876f5e2cbdc26ee799859a5
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-18 13:12:15 -0800

    Update generate number less than target.py

--- generate number less than target.py
+++ generate number less than target.py
@@ -1,3 +1,5 @@
 给你一个数字的list，然后给一个target，求所有由list里面数字组合成的小于target的数，数字可以重复使用 
 比如list是{3,7,8}，target是8700，然后3, 8, 7 ... 333, 888, ... , 8377 等等都算 
 这题典型的backtrack, 然后对面表示对的，这才是正确的方法
+
+https://instant.1point3acres.com/thread/197857

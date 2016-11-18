f592b00adec50e91cb424f8a4085ff5f52f3c072
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-18 11:44:35 -0800

    Create print message - topological sort.py

---
+++ print message - topological sort.py
@@ -0,0 +1,4 @@
+给了list of message， 每message有一个parentID， ID 和content。 
+要求输入list of message按照先parent后children的顺序， 给定root message的parentID是固定的字符串。其实就是topological sort。 
+写完了之后简单跑了testcase问了时间复杂度就完了。 
+
